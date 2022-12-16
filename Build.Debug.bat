8e0f16aca43c6a2204994f875f78715ea509df20
Piero Viano <piero.viano@gmail.com>
2022-12-16 12:51:57 +0200

    ''

--- Build.Debug.bat
+++ Build.Debug.bat
@@ -2,7 +2,7 @@ mklink /j Packages ..\WhenTheVersion\Packages
 del ..\WhenTheVersion\Packages\PdfCompressorLibrary.*
 rmdir /s /q %userprofile%\.nuget\Packages\PdfCompressorLibrary
 nuget restore PdfCompressorLibrary.sln
-MSBuild.exe PdfCompressorLibrary.sln /property:Configuration=Debug 
+MSBuild.exe PdfCompressorLibrary.sln -m /property:Configuration=Debug 
 copy "PdfCompressorLibrary\bin\Debug\PdfCompressorLibrary.1.0.0.nupkg" ..\WhenTheVersion\Packages\
 copy "PdfCompressorLibrary\bin\Debug\PdfCompressorLibrary.1.0.0.symbols.nupkg" ..\WhenTheVersion\Packages\
 git add -A
