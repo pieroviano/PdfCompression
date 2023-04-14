8f16aa40ed1b621ff71563133fb7995fc07a5f96
Piero Viano <piero.viano@gmail.com>
2023-04-14 03:06:32 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 IF NOT DEFINED Configuration SET Configuration=Release
+MSBuild.exe PdfCompressorLibrary.sln -t:clean
 MSBuild.exe PdfCompressorLibrary.sln -t:restore -p:RestorePackagesConfig=true
 MSBuild.exe PdfCompressorLibrary.sln -m /property:Configuration=%Configuration%
 git add -A
